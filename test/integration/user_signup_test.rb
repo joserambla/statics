df5f150050a9b938e17e9d883a7ab401efc9683b
Jose Rambla <jose.rambla@gmail.com>
2016-03-23 14:14:16 +0100

    Finished signup

---
+++ test/integration/user_signup_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class UserSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "wrong signup does not create user" do
+  	get signup_path
+  	before = User.count
+  	post users_path, user: { name: "Pepe perez", 
+  								email: "mail@mail.com",
+  								password: "foo",
+  								password_confirmation: "foo"
+  							}
+  	after = User.count
+  	assert_equal before, after
+  	assert_template "users/new"
+  end
+end
